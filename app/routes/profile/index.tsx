d0fbab3d5dda8fcd7ebcb352594fc26d431c568a
Ella Tanttu <ellajtanttu@gmail.com>
2022-04-07 10:48:03 -0700

    add profile and trips directories to routes branch
    Co-authored-by:  Kim Brannian <brannian@gmail.com>
    Co-authored-by: Jeff Lai <pjefflai@gmail.com> Co-authored-by: Jacob Wilson <jacobleeeugenewilson@gmail.com>

---
+++ app/routes/profile/index.tsx
@@ -0,0 +1,56 @@
+import type { FC } from "react"
+
+import { Link } from "remix"
+
+import { join } from "~/utils"
+
+const Index: FC = () => {
+  return (
+    <div>
+      <Link
+        to="/trips"
+        className={join(
+          `flex`,
+          `items-center`,
+          `justify-center`,
+          `rounded-md`,
+          `border`,
+          `border-transparent`,
+          `bg-white`,
+          `px-4`,
+          `py-3`,
+          `text-base`,
+          `font-medium`,
+          `text-yellow-700`,
+          `shadow-sm`,
+          `hover:bg-yellow-50`,
+          `sm:px-8`,
+        )}
+      >
+        Trips
+      </Link>
+      <Link
+        to="/edit"
+        className={join(
+          `flex`,
+          `items-center`,
+          `justify-center`,
+          `rounded-md`,
+          `border`,
+          `border-transparent`,
+          `bg-white`,
+          `px-4`,
+          `py-3`,
+          `text-base`,
+          `font-medium`,
+          `text-yellow-700`,
+          `shadow-sm`,
+          `hover:bg-yellow-50`,
+          `sm:px-8`,
+        )}
+      >
+        Edit Profile
+      </Link>
+    </div>
+  )
+}
---
+++ app/routes/trips/index.tsx
@@ -0,0 +1,34 @@
+import type { FC } from "react"
+
+import { Link } from "remix"
+
+import { join } from "~/utils"
+
+const Index: FC = () => {
+  return (
+    <div>
+      <Link
+        to="/new"
+        className={join(
+          `flex`,
+          `items-center`,
+          `justify-center`,
+          `rounded-md`,
+          `border`,
+          `border-transparent`,
+          `bg-white`,
+          `px-4`,
+          `py-3`,
+          `text-base`,
+          `font-medium`,
+          `text-yellow-700`,
+          `shadow-sm`,
+          `hover:bg-yellow-50`,
+          `sm:px-8`,
+        )}
+      >
+        Create Trip
+      </Link>
+    </div>
+  )
+}
