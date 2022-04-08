239d4a8b6e5731a29555456ae4397e799179bcd0
Kim Brannian <brannian@gmail.com>
2022-04-08 15:22:39 -0700

    initialize trips with a nickName
    Co-authored-by:  Ella Tanttu <ellajtanttu@gmail.com>
    Co-authored-by:  Jeff Lai <pjefflai@gmail.com>
    Co-authored-by: Jacob Wilson
    <jacobleeeugenewilson@gmail.com>

--- app/routes/trips/new.tsx
+++ app/routes/trips/new.tsx
@@ -1,15 +1,70 @@
 import type { FC } from "react"
 
-import { Link } from "remix"
+import type { ActionFunction } from "remix"
+import { Link, json, Form, redirect, useActionData } from "remix"
 
+import invariant from "tiny-invariant"
+
+import { createTrip } from "~/models/trip.server"
+import { requireUserId } from "~/session.server"
 import { join } from "~/utils"
 
+type ActionData =
+  | {
+      nickName: string | null
+    }
+  | undefined
+
+export const action: ActionFunction = async ({ request }) => {
+  const ownerId = await requireUserId(request)
+  const formData = await request.formData()
+
+  const nickName = formData.get(`nickName`)
+
+  const errors: ActionData = {
+    nickName: nickName ? null : `nickName is required`,
+  }
+
+  const hasErrors = Object.values(errors).some((errorMessage) => errorMessage)
+  if (hasErrors) {
+    return json<ActionData>(errors)
+  }
+
+  invariant(typeof nickName === `string`, `nickName must be a string`)
+
+  await createTrip({ nickName, ownerId })
+  return redirect(`/trips`)
+}
+
 const NewTrip: FC = () => {
+  const errors = useActionData()
+  const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`
+
   return (
     <div>
       <h1 className={join(`flex`, `items-center`, `justify-center`)}>
         New Trip Form
       </h1>
+      <Form method="post">
+        <p>
+          <label>
+            Name Your Trip:{` `}
+            {errors?.nickName ? (
+              <em className="text-red-600">{errors.nickName}</em>
+            ) : null}
+            <input type="text" name="nickName" className={inputClassName} />
+          </label>
+        </p>
+        <p className="text-right">
+          <button
+            type="submit"
+            className="rounded bg-blue-500 py-2 px-4 
+            text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          >
+            Create Trip
+          </button>
+        </p>
+      </Form>
       <Link
         to="/trips"
         className={join(
