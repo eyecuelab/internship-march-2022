d94f77954bfa7bac4d507edade724a4b9f7770eb
Ella Tanttu <ellajtanttu@gmail.com>
2022-04-12 16:14:34 -0700

    WIP: seeding database, errors on seed command
    Co-authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Jeff Lai <pjefflai@gmail.com>
    Co-authored-by: Jacob Wilson <jacobleeeugenewilson@gmail.com>

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client"
+import type { Trip, User, Attendee, Password } from "@prisma/client"
 import bcrypt from "bcryptjs"
-
 const prisma = new PrismaClient()
 
 async function seed() {
@@ -24,7 +24,116 @@ async function seed() {
     },
   })
 
+  await Promise.all(
+    getTrips().map((trip) => {
+      return prisma.trip.create({ data: trip })
+    }),
+  )
+
+  await Promise.all(
+    getUsers().map((user) => {
+      return prisma.user.create({ data: user })
+    }),
+  )
+  await Promise.all(
+    getAttendees().map((attendee) => {
+      return prisma.attendee.create({ data: attendee })
+    }),
+  )
+
   console.log(`Database has been seeded. 🌱`)
+}
+
+const getTrips = (): Trip[] => {
+  return [
+    {
+      id: `1`,
+      ownerId: `1`,
+      startDate: null,
+      endDate: null,
+      nickName: `testTrip1`,
+      createdAt: new Date(0),
+      updatedAt: new Date(0),
+    },
+    {
+      id: `2`,
+      ownerId: `2`,
+      startDate: null,
+      endDate: null,
+      nickName: `testTrip2`,
+      createdAt: new Date(0),
+      updatedAt: new Date(0),
+    },
+    {
+      id: `3`,
+      ownerId: `3`,
+      startDate: null,
+      endDate: null,
+      nickName: `testTrip3`,
+      createdAt: new Date(0),
+      updatedAt: new Date(0),
+    },
+  ]
+}
+
+const getUsers = (): User[] => {
+  return [
+    {
+      id: `1`,
+      email: `testEmail1`,
+      userName: `one`,
+      avatarUrl: `testurlone`,
+      createdAt: new Date(0),
+      updatedAt: new Date(0),
+    },
+    {
+      id: `2`,
+      email: `testEmail2`,
+      userName: `two`,
+      avatarUrl: `testurltwo`,
+      createdAt: new Date(0),
+      updatedAt: new Date(0),
+    },
+    {
+      id: `3`,
+      email: `testEmail3`,
+      userName: `three`,
+      avatarUrl: `testurlthree`,
+      createdAt: new Date(0),
+      updatedAt: new Date(0),
+    },
+  ]
+}
+const getAttendees = (): Attendee[] => {
+  return [
+    {
+      tripId: `1`,
+      userId: `1`,
+      deciderId: `1`,
+      packingListId: `1`,
+      isAccepted: null,
+      createdAt: new Date(0),
+      updatedAt: new Date(0),
+    },
+    {
+      tripId: `2`,
+      userId: `2`,
+      deciderId: `2`,
+      packingListId: `2`,
+      isAccepted: null,
+      createdAt: new Date(0),
+      updatedAt: new Date(0),
+    },
+    {
+      tripId: `3`,
+      userId: `3`,
+      deciderId: `3`,
+      packingListId: `3`,
+      isAccepted: null,
+      createdAt: new Date(0),
+      updatedAt: new Date(0),
+    },
+  ]
 }
 
 seed()
