04b5ef97a0028b4a501431816ac3b829a4640232
Kim Brannian <brannian@gmail.com>
2022-04-19 11:23:51 -0700

    refactor expenses

--- app/routes/trips/$tripId.expenses.tsx
+++ app/routes/trips/$tripId.expenses.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react"
-
+import type { Params } from "react-router"
 import { LoaderFunction, Link, json, useLoaderData, useParams, useSearchParams } from "remix"
  
 import invariant from "tiny-invariant"
@@ -8,38 +8,29 @@ import { getExpensesByTripId } from "~/models/expense.server"
 import { getAttendeesByTripId } from "~/models/attendee.server"
 import { join } from "~/utils"
 
-// interface FullAttendee extends Attendee {
-//   user: User;
-//   expenses: Expense[]
-// }
-//Should we have a utils folder for interfaces to import when we need them or just include them in each component
-//with properties as we need them? 
 
 
-// type LoaderData= { 
-//   attendees: Attendee[]
-// }
+type LoaderData =  Awaited<ReturnType<typeof getLoaderData>>;
 
+const getLoaderData = async (params: Params<string>) => {
+  invariant(params.tripId, `trips required`)
+  return await getAttendeesByTripId(params.tripId)
+}
 export const loader: LoaderFunction = async({params}) => {
   invariant(params.tripId, `tripId is required`)
-  const attendees = await getAttendeesByTripId(params.tripId)
-  return json({
-  attendees
-  },)
+  return json<LoaderData>(await getLoaderData(params))
 }
 
 
 const Expenses: FC = () => {
-  const { attendees } = useLoaderData()
-  console.log({attendees})
+  const data = useLoaderData<LoaderData>() 
 
   return (
     <div>
       <h1 className={join(`flex`, `items-center`, `justify-center`)}>
         Expenses
-        
       </h1>
-      {attendees.map((attendee)=> (
+      {data.map((attendee)=> (
     <div key={attendee.tripId}>
       <h1>{attendee.user.userName}</h1>
       <ul>
