63d9227af024af6ba816ac8490439e36afe5649a
Kim Brannian <brannian@gmail.com>
2022-04-20 11:24:47 -0700

    update attendee server

--- app/models/expense.server.ts
+++ app/models/expense.server.ts
@@ -1,14 +1,15 @@
 import type {Expense, User, Trip, Attendee, Prisma } from "@prisma/client"
 
+
 import { prisma } from "../db.server"
 
 export type { Expense } from "@prisma/client"
 
-export async function createExpense(expense: Pick<Expense, 
-  `description`| `total`| `tripId` | `userId`>,):
-  Promise<Attendee> {
-  return prisma.attendee.create({data: expense })
+export async function createExpense(expense: Pick<Expense, `description`| `total`| `tripId` | `userId`>) {
+  return prisma.expense.create({data: expense })
   }
+
+
 
   export async function getExpensesByTripId(tripId: Trip[`id`]) { 
     return prisma.expense.findMany({ where: { tripId: tripId } })
