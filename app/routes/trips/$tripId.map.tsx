95a46eac3887d96a5d94389a331a138a4eb89ae7
Jeff Lai <pjefflai@gmail.com>
2022-04-20 11:27:58 -0700

    successful api call of embedded map

--- app/routes/trips/$tripId.map.tsx
+++ app/routes/trips/$tripId.map.tsx
@@ -1,10 +1,34 @@
 import type { FC } from "react"
 
-import { Link } from "remix"
+import { Link, LoaderFunction, useLoaderData, json } from "remix"
 
 import { join } from "~/utils"
 
+import { Params } from "react-router-dom"
+
+
+
+type LoaderData = Awaited<ReturnType<typeof getLoaderData>>;
+
+const getLoaderData = async (request: Request, params: Params<string>) => {
+  const url = `https://www.google.com/maps/embed/v1/view?zoom=10&center=45.5152%2C-122.6784&key=${process.env.REACT_APP_MAP_API}`
+  console.log(process.env);
+
+  return {
+    url: url
+    
+  }
+}
+
+export const loader: LoaderFunction = async ({
+  request,
+  params
+}) => {
+
+  return json<LoaderData>(await getLoaderData(request, params))
+}
 const Map: FC = () => {
+  const data = useLoaderData<LoaderData>();
   return (
     <div>
       <h1 className={join(`flex`, `items-center`, `justify-center`)}>Map</h1>
@@ -52,6 +76,13 @@ const Map: FC = () => {
       >
         Return to profile
       </Link>
+      <iframe 
+      width="600"
+      height="450"
+      loading="lazy"
+      src={data.url}
+      >
+      </iframe>
     </div>
   )
 }
