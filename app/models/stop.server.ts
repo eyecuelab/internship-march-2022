3c0083a9bdb059cdcc7b4263f66d79f1eb1df49a
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2022-05-03 15:11:20 -0800

    add deleteStop, updateIndex

--- app/models/stop.server.ts
+++ app/models/stop.server.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import type { Stop, Trip } from "@prisma/client"
+import invariant from "tiny-invariant"
 
 import { prisma } from "../db.server"
 
@@ -38,6 +39,43 @@ export async function updateStop(
       apiResult: stop.apiResult,
       index: stop.index,
       tripId: stop.tripId,
+    },
+  })
+}
+
+export async function updateStopIndex(id: Stop[`id`], tripId: Stop[`tripId`]) {
+  const stop = await prisma.stop.findFirst({
+    where: {
+      id,
+    },
+  })
+  invariant(stop, `did not find a valid stop`)
+  const stopsToUpdate = await prisma.stop.findMany({
+    where: {
+      tripId,
+      index: {
+        gt: stop.index,
+      },
+    },
+  })
+  stopsToUpdate.map(async (s) => {
+    return await prisma.stop.update({
+      where: {
+        id: s.id,
+      },
+      data: {
+        index: s.index - 1,
+      },
+    })
+  })
+  return stopsToUpdate
+}
+
+export async function deleteStopById(id: Stop[`id`], tripId: Stop[`tripId`]) {
+  await updateStopIndex(id, tripId)
+  return prisma.stop.delete({
+    where: {
+      id,
     },
   })
 }
