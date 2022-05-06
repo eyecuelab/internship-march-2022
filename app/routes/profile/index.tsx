41e4a26d0e1240cec5206be17ee663b17312e93c
Ella Tanttu <88292299+ellajtanttu@users.noreply.github.com>
2022-05-06 16:17:44 -0700

    fix errors on profile/index, functionality and styling

--- app/routes/profile/index.tsx
+++ app/routes/profile/index.tsx
@@ -35,7 +35,7 @@ type LoaderData = Awaited<ReturnType<typeof getLoaderData>>
 const getLoaderData = async (request: Request) => {
   const userId = await requireUserId(request)
   const user = await getUserById(userId)
-  // const attendeeTrips = user.attendees
+  const attendeeTrips = user.attendees
   // console.log(attendeeTrips[0].tripId)
   //--------------
   const tripList = attendeeTrips.map(async (item: Attendee) => {
@@ -74,53 +74,22 @@ const Index: FC = () => {
   console.log(avatar)
 
   //Dates
-  const convertStringToDate = (inputDate: Date | string | null | undefined) => {
-    return (inputDate ? new Date(inputDate) : new Date()).toLocaleDateString()
+  const convertStringToDate = (inputDate: string) => {
+    return (inputDate ? new Date(inputDate) : new Date(0)).toLocaleDateString()
   }
   const createdDate = data.user?.createdAt
   const profileCreatedDate = convertStringToDate(createdDate)
 
   //Attendees
   const attendees = data?.user?.attendees
-  const attendeesCount = attendees?.length
+  // console.log(attendees)
+  const attendeesCount = attendees.length
 
   return (
-    <div className="flex-col">
-      <div className={join(`ml-8`)}>
-        <SvgBackButton />
-      </div>
-
-      <div className={join(`flex`, `justify-between`, `items-center`)}>
-        <div>
-          <Header>Your Account</Header>
-        </div>
-        <div className="mx-8">
-          <Outlet />
-          <Link to={`edit`}>
-            <span className={join(`flex`, `m-8`)}>
-              <SvgGear />
-            </span>
-          </Link>
-        </div>
-      </div>
-
-      <div className={join(`container`, `mx-auto`, `-space-y-40`)}>
-        <div className={join(`flex`, `justify-center`, `mb-10`)}>
-          <SvgProfileDial />
-        </div>
-        <div className={join(`text-center`)}>
-          <ProHugeNumber>{trips?.length}</ProHugeNumber>
-          <ProH4>Total Trips</ProH4>
-          <ProBody>
-            Member Since{` `}
-            {profileCreatedDate}
-          </ProBody>
-        </div>
-      </div>
-
-      <div className={join(`container`, `mx-auto`, `mt-20`)}>
-        <div className={join(`text-center`, `-ml-20`, `p-8`)}>
-          <SubHeader>Recent Trips</SubHeader>
+    <>
+      <div className="flex-col">
+        <div className={join(`ml-8`)}>
+          <SvgBackButton />
         </div>
 
         <div className={join(`flex`, `justify-between`, `items-center`)}>
@@ -137,99 +106,89 @@ const Index: FC = () => {
                   </span>
                 </Link> */}
 
-                <ul>
-                  {trips?.map((trip: Trip) => (
-                    <div
-                      key={trip.id}
-                      className={join(`flex`, `items-center`, `mb-20`, `justify-center`)}
-                    >
-                      <div className={join(`order-1`)}>
-                        <ProTripImage>
-                          <SvgTrip />
-                        </ProTripImage>
-                      </div>
-                      <div className={join(`order-2`, `ml-5`)}>
-                        <ProH4>{trip.nickName}</ProH4>
-                        <ProBody>
-                          {convertStringToDate(trip.startDate)}
-                          {` `}
-                          {trip.startDate !== null ? `–` : ``}
-                          {` `}
-                          {convertStringToDate(trip.endDate)}
-                        </ProBody>
-                        <ProBody>
-                          {attendeesCount}
-                          {` `}Travelers
-                        </ProBody>
-                      </div>
-                    </div>
-                  </div>
+                <Form action="/logout" method="post">
+                  <button
+                    type="submit"
+                    className={join(`rounded`, `py-2`, `px-4`, `text-#CAD2C5`)}
+                  >
+                    Logout
+                  </button>
+                </Form>
+              </AddButtonText>
+              <Link to={`edit`}>
+                <span className={join(`flex`, `m-8`)}>
+                  <SvgGear />
+                </span>
+              </Link>
             </div>
+          </div>
+        </div>
 
-            <div className={join(`container`, `mx-auto`, `-space-y-60`)}>
-              <div className={join(`flex`, `justify-center`, `mb-10`)}>
-                <SvgProfileDial />
-              </div>
+        <div className={join(`container`, `mx-auto`, `-space-y-60`)}>
+          <div className={join(`flex`, `justify-center`, `mb-10`)}>
+            <SvgProfileDial />
+          </div>
+          <div
+            className={join(`flex-col`, `text-center`, `justify-content-center`)}
+          >
+            <div className={join(`flex`, `justify-center`, `pt-6`)}>
+              <ProfileAvatarMain src={avatar} />
+            </div>
+            <div>
+              <ProHugeNumber>{attendees.length}</ProHugeNumber>
+              <ProH4>Total Trips</ProH4>
+              <ProBody>
+                Member Since{` `}
+                {profileCreatedDate}
+              </ProBody>
+            </div>
+          </div>
+        </div>
+
+        <div className={join(`container`, `mx-auto`, `mt-8`)}>
+          <div className={join(`text-center`, `-ml-20`, `p-8`)}>
+            <SubHeader>Recent Trips</SubHeader>
+          </div>
+
+          <ul>
+            {trips.map((trip: Trip) => (
               <div
-                className={join(`flex-col`, `text-center`, `justify-content-center`)}
+                key={trip.id}
+                className={join(
+                  `flex`,
+                  `items-center`,
+                  `mb-20`,
+                  `justify-center`,
+                )}
               >
-                <div className={join(`flex`, `justify-center`, `pt-6`)}>
-                  <ProfileAvatarMain src={avatar} />
+                <div className={join(`order-1`)}>
+                  <ProTripImage>
+                    <SvgTrip />
+                  </ProTripImage>
                 </div>
-                <div>
-                  <ProHugeNumber>{attendees.length}</ProHugeNumber>
-                  <ProH4>Total Trips</ProH4>
+                <div className={join(`order-2`, `ml-5`)}>
+                  <ProH4>{trip.nickName}</ProH4>
                   <ProBody>
-                    Member Since{` `}
-                    {profileCreatedDate}
+                    {convertStringToDate(trip.startDate)}
+                    {` `}
+                    {trip.startDate !== null ? `–` : ``}
+                    {` `}
+                    {convertStringToDate(trip.endDate)}
+                  </ProBody>
+                  <ProBody>
+                    {attendeesCount}
+                    {` `}Travelers
                   </ProBody>
                 </div>
               </div>
-            </div>
-
-            <div className={join(`container`, `mx-auto`, `mt-8`)}>
-              <div className={join(`text-center`, `-ml-20`, `p-8`)}>
-                <SubHeader>Recent Trips</SubHeader>
-              </div>
-
-              <ul>
-                {trips.map((trip: Trip) => (
-                  <div
-                    key={trip.id}
-                    className={join(
-                      `flex`,
-                      `items-center`,
-                      `mb-20`,
-                      `justify-center`,
-                    )}
-                  >
-                    <div className={join(`order-1`)}>
-                      <ProTripImage>
-                        <SvgTrip />
-                      </ProTripImage>
-                    </div>
-                    <div className={join(`order-2`, `ml-5`)}>
-                      <ProH4>{trip.nickName}</ProH4>
-                      <ProBody>
-                        {convertStringToDate(trip.startDate)}
-                        {` `}
-                        {trip.startDate !== null ? `–` : ``}
-                        {` `}
-                        {convertStringToDate(trip.endDate)}
-                      </ProBody>
-                      <ProBody>
-                        {attendeesCount}
-                        {` `}Travelers
-                      </ProBody>
-                    </div>
-                  </div>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <NavBar />
+            ))}
+          </ul>
         </div>
-        )
+
+        <NavBar />
+      </div>
+    </>
+  )
 }
 
-        export default Index
+export default Index
