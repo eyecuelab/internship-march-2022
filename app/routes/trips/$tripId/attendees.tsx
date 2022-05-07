1149dbff19f9785222722303f2ca2d1cdaeed7a9
Jeff Lai <pjefflai@gmail.com>
2022-05-07 14:44:56 -0700

    add all necessary svg to attendees.tsx

--- app/routes/trips/$tripId/attendees.tsx
+++ app/routes/trips/$tripId/attendees.tsx
@@ -9,7 +9,12 @@ import invariant from "tiny-invariant"
 import { getAttendeesByTripId } from "~/models/attendee.server"
 import { getTripById } from "~/models/trip.server"
 import { TitleText, Avatar, RoundedRectangle } from "~/styles/styledComponents"
-import { join } from "~/utils"
+import SvgCoins from "~/styles/SVGR/SvgCoins"
+import SvgDice from "~/styles/SVGR/SvgDice"
+import SvgSvgHorizontalLine from "~/styles/SVGR/SvgHorizontalLine"
+import SvgLuggage from "~/styles/SVGR/SvgLuggage"
+import SvgVerticalLine from "~/styles/SVGR/SvgVerticalLine"
+import { formatTrip, join } from "~/utils"
 
 type LoaderData = Awaited<ReturnType<typeof getLoaderData>>
 
@@ -19,12 +24,13 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 const getLoaderData = async (request: Request, params: Params<string>) => {
   const { tripId } = params
   invariant(tripId, `must have tripId`)
-  const trip = getTripById(tripId)
+  const tempTrip = await getTripById(tripId)
   const attendees = await getAttendeesByTripId(tripId)
-
+  invariant(tempTrip, `need tripId`)
+  const trip = formatTrip(tempTrip)
   invariant(attendees, `need attendeesId`)
   return { trip, attendees }
-  // console.log(getTripById)
+  console.log(tripId)
 }
 
 // tripId, attendees,start and end dates, start and end location, stops[]
@@ -33,34 +39,45 @@ const AttendeesLayout: FC = () => {
   const data = useLoaderData<LoaderData>()
 
   console.log(data)
-  const linkStyles = [
-    `flex`,
-    `items-center`,
-    `justify-center`,
-    `rounded-md`,
-    `border`,
-    `border-transparent`,
-    `bg-white`,
-    `px-4`,
-    `py-3`,
-    `text-base`,
-    `font-medium`,
-    `text-yellow-700`,
-    `shadow-sm`,
-    `hover:bg-yellow-50`,
-    `sm:px-8`,
-  ]
+
   const defaultAvatar = `public/img/default-avatar.jpg`
   const rectangleStyles = [`flex`, `mx-2`]
   const avatarDivStyles = [`ml-2`, `flex`]
-  const titleDivStyles = [`ml-4`, `text-left`, `flex-1`]
+  const titleDivStyles = [`ml-4`, `flex-1`]
 
   return (
     <div>
       <div>
         <div>
-          <RoundedRectangle className={join(...rectangleStyles)}>
-            <div className={join(`flex-col`)}>
+          <RoundedRectangle className={join(...rectangleStyles, `flex-col`)}>
+            <div className={join(`flex`, `content-start`)}>
+              <div className={join(`pr-10`, `ml-8`, `mb-3`)}>
+                <TitleText className={join(`mb-3`)}>Starts</TitleText>
+                <TitleText>
+                  {data.trip.startDate ? data.trip.startDate : `00/00/00`}
+                </TitleText>
+                <TitleText>
+                  {data.trip.stops[0]
+                    ? data.trip.stops[-1].apiResult.name
+                    : `TBD`}
+                </TitleText>
+              </div>
+              <SvgVerticalLine />
+
+              <div className={join(`pl-10`, `ml-6`)}>
+                <TitleText className={join(`mb-3`)}>Ends</TitleText>
+                <TitleText>
+                  {data.trip.endDate ? data.trip.endDate : `00/00/00`}
+                </TitleText>
+                <TitleText>
+                  {data.trip.stops[-1]
+                    ? data.trip.stops[-1].apiResult.name
+                    : `TBD`}
+                </TitleText>
+              </div>
+            </div>
+            <SvgSvgHorizontalLine />
+            <div className={join(`mt-3`, `flex-col`, `mt-2px`)}>
               <TitleText>
                 <div className={join(`ml-8`, `mb-3`)}>Travelers</div>
               </TitleText>
@@ -85,18 +102,20 @@ const AttendeesLayout: FC = () => {
           </RoundedRectangle>
         </div>
       </div>
-      <Link to="edit/" className={join()}>
-        Edit
-      </Link>
-      <Link to="packing-list/" className={join(...linkStyles)}>
-        Packing List
-      </Link>
-      <Link to="expenses/" className={join(...linkStyles)}>
-        Expenses
-      </Link>
-      <Link to="decider/" className={join(...linkStyles)}>
-        Decider
-      </Link>
+      <RoundedRectangle className={join(...rectangleStyles, `flex-col`)}>
+        <Link to="packing-list/" className={join()}>
+          <SvgLuggage />
+          Packing List
+        </Link>
+        <Link to="expenses/" className={join()}>
+          <SvgCoins />
+          Expenses
+        </Link>
+        <Link to="decider/" className={join()}>
+          <SvgDice />
+          Decider
+        </Link>
+      </RoundedRectangle>
     </div>
   )
 }
